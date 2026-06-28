63acbdca6ac21d5256bb61b80316e798b6fb43ce
irrationalpie7 <irrationalpie7@gmail.com>
2026-06-28 11:21:19 -0700

    wait until first script loads

--- tampermonkey/pause-gifs.pub.user.js
+++ tampermonkey/pause-gifs.pub.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AO3 pause gifs
-// @version      0.4
+// @version      0.5
 // @description  Pause gifs with accessible "play" button
 // @author       irrationalpie7
 // @match        https://archiveofourown.org/*
@@ -14,19 +14,21 @@
   "use strict";
 
   const importScript = document.createElement("script");
+  importScript.id = "gifa11y-import-script";
   importScript.src =
     "https://cdn.jsdelivr.net/gh/adamchaboryk/gifa11y@2.2.2/dist/js/gifa11y.umd.min.js";
-  importScript.defer = true;
   document.head.appendChild(importScript);
 
   const setupScript = document.createElement("script");
-  setupScript.innerHTML = `const gifa11y = new Gifa11y({
-    container: "main",
-    buttonBackground: "#000000",
-    buttonBackgroundHover: "#404040",
-    buttonIconColor: "white",
+  setupScript.innerHTML = `const importScript = document.getElementById("gifa11y-import-script");
+  importScript.addEventListener("load", () => {
+    const gifa11y = new Gifa11y({
+      container: "main",
+      buttonBackground: "#000000",
+      buttonBackgroundHover: "#404040",
+      buttonIconColor: "white",
+    });
   });`;
-  setupScript.defer = true;
   document.head.appendChild(setupScript);
 
   const images = Array.from(document.querySelectorAll("img"));
