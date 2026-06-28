e09170b0b13f8f9e5edb96259da6d1455c83b8c5
irrationalpie7 <76858438+irrationalpie7@users.noreply.github.com>
2026-06-28 11:47:50 -0700

    Add tamperscript for pausing gifs (#2)
    * First attempt at pausable gifs tamperscript
    
    * Attempt 2
    
    * Tweak again
    
    * v0.3
    
    * .4
    
    * wait until first script loads
    
    * logs
    
    * Finally
    
    * Update readme

---
+++ tampermonkey/pause-gifs.pub.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         AO3 pause gifs
+// @version      1.0
+// @description  Pause gifs with accessible "play" button
+// @author       irrationalpie7
+// @match        https://archiveofourown.org/*
+// clang-format off
+// @updateURL    https://github.com/irrationalpie7/fandom-scripts/raw/main/tampermonkey/pause-gifs.pub.user.js
+// @downloadURL  https://github.com/irrationalpie7/fandom-scripts/raw/main/tampermonkey/pause-gifs.pub.user.js
+// clang-format on
+// ==/UserScript==
+
+(async () => {
+  "use strict";
+
+  const setupScript = document.createElement("script");
+  setupScript.type = "module";
+  setupScript.innerHTML = `import Gifa11y from "https://cdn.jsdelivr.net/gh/adamchaboryk/gifa11y@2.2.2/dist/js/gifa11y.esm.min.js";
+  const gifa11y = new Gifa11y({
+    container: 'main',
+    buttonBackground: '#000000',
+    buttonBackgroundHover: '#404040',
+    buttonIconColor: 'white',
+    missingAltWarning: false
+  });
+  window.gifa11y = gifa11y;
+  setTimeout(() => {
+    gifa11y.findNew();
+  }, 3_000);`;
+  document.head.appendChild(setupScript);
+
+  const images = Array.from(document.querySelectorAll("img"));
+  images.forEach((image) => image.classList.add("gifa11y-paused"));
+})();
