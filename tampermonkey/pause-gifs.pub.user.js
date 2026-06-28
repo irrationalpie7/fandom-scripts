6c371554e7364f20c14eb9f59e1639c595ea9016
irrationalpie7 <irrationalpie7@gmail.com>
2026-06-28 10:32:20 -0700

    Attempt 2

--- tampermonkey/pause-gifs.pub.user.js
+++ tampermonkey/pause-gifs.pub.user.js
@@ -1,25 +1,31 @@
 // ==UserScript==
 // @name         AO3 pause gifs
-// @version      1.0
+// @version      1.1
 // @description  Pause gifs with accessible "play" button
 // @author       irrationalpie7
 // @match        https://archiveofourown.org/*
 // clang-format off
 // @updateURL    https://github.com/irrationalpie7/fandom-scripts/raw/main/tampermonkey/pause-gifs.pub.user.js
 // @downloadURL  https://github.com/irrationalpie7/fandom-scripts/raw/main/tampermonkey/pause-gifs.pub.user.js
-// @require      https://cdn.jsdelivr.net/gh/adamchaboryk/gifa11y@2.2.2/dist/js/gifa11y.umd.min.js
 // clang-format on
 // ==/UserScript==
 
 (async () => {
   "use strict";
 
+  const importScript = document.createElement("script");
+  importScript.src =
+    "https://cdn.jsdelivr.net/gh/adamchaboryk/gifa11y@2.2.2/dist/js/gifa11y.umd.min.js";
+
+  const setupScript = document.createElement("script");
+  setupScript.innerHTML = ```
   const gifa11y = new Gifa11y({
     container: "main",
     buttonBackground: "#000000",
     buttonBackgroundHover: "#404040",
     buttonIconColor: "white",
-  });
+  });```;
+  document.head.append([importScript, setupScript]);
 
   const images = Array.from(document.querySelectorAll("img"));
   images.forEach((image) => image.classList.add("gifa11y-paused"));
